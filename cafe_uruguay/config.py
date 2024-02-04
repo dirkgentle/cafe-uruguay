d760a8b900eb24c74bf7aca76299754a0f96f33c
DirkGentle <35881987+dirkgentle@users.noreply.github.com>
2024-02-04 13:34:15 -0500

    Bot implementation

---
+++ cafe_uruguay/config.py
@@ -0,0 +1,31 @@
+import os
+
+
+class AuthConfig:
+    password = os.getenv("REDDIT_PWD")
+    username = os.getenv("REDDIT_USERNAME")
+    client_id = os.getenv("REDDIT_CLIENT_ID")
+    client_secret = os.getenv("REDDIT_CLIENT_SECRET")
+
+
+class BasicConfig:
+    debug_mode = bool(os.getenv("REDDIT_DEBUG"))
+    subreddit = os.getenv("REDDIT_SUBREDDIT", "Uruguay" if not debug_mode else "test")
+
+    title = "Café Uruguay"
+
+    body = (
+        "Bienvenidos y bienvenidas al café de /r/Uruguay, un espacio semanal"
+        " para relajarse y charlar. Vayan tomando asiento, en un momento les"
+        " llevamos la orden."
+        "\n\n*****\n\n"
+        " *En estos posts no aplica la regla de contenido relacionado con Uruguay.*"
+        " Pueden hablar de lo que quieran,"
+        " mientras no rompa ninguna otra regla de Reddit o del sub."
+        " Prohibido salivar y hablar con el conductor."
+        " \n\n*****\n\n"
+        " *Bot by \\/u/DirkGentle.*"
+        " [Source.](https://github.com/dirkgentle/cafe-uruguay)"
+    )
+
+    flair_text = os.getenv("REDDIT_FLAIR", "AskUruguay 🧉")
--- cafe_uruguay/main.py
+++ cafe_uruguay/main.py
@@ -1,1 +1,38 @@
-print("Hi!")
+from datetime import date
+
+import praw
+from praw.models import Subreddit
+
+from cafe_uruguay.config import AuthConfig, BasicConfig
+
+
+def _main(debug: bool = False):
+    title = f"{BasicConfig.title} {date.today()}"
+    body = BasicConfig.body
+
+    reddit_client = praw.Reddit(
+        client_id=AuthConfig.client_id,
+        client_secret=AuthConfig.client_secret,
+        password=AuthConfig.password,
+        username=AuthConfig.username,
+        user_agent="script for /u/random_daily_subject",
+    )
+
+    subreddit = reddit_client.subreddit(BasicConfig.subreddit)
+
+    flair_id = get_flair_id(subreddit, BasicConfig.flair_text)
+
+    _ = subreddit.submit(title, selftext=body, flair_id=flair_id)
+
+
+def get_flair_id(subreddit: Subreddit, text: str) -> str:
+    return next(
+        t["id"]
+        for t in subreddit.flair.link_templates
+        if t["type"] == "richtext"
+        and any(chunk.get("t") == text for chunk in t["richtext"])
+    )
+
+
+if __name__ == "__main__":
+    _main()
