e26419e2398d6266d4c7f05dc2601565d2cfc88b
DirkGentle <35881987+dirkgentle@users.noreply.github.com>
2024-02-04 13:42:34 -0500

    Give default if no flair is found

--- cafe_uruguay/main.py
+++ cafe_uruguay/main.py
@@ -1,6 +1,7 @@
 from datetime import date
 
 import praw
+import prawcore
 from praw.models import Subreddit
 
 from cafe_uruguay.config import AuthConfig, BasicConfig
@@ -20,17 +21,23 @@ def _main(debug: bool = False):
 
     subreddit = reddit_client.subreddit(BasicConfig.subreddit)
 
-    flair_id = get_flair_id(subreddit, BasicConfig.flair_text)
+    try:
+        flair_id = get_flair_id(subreddit, BasicConfig.flair_text)
+    except prawcore.exceptions.Forbidden:
+        flair_id = None
 
     _ = subreddit.submit(title, selftext=body, flair_id=flair_id)
 
 
-def get_flair_id(subreddit: Subreddit, text: str) -> str:
+def get_flair_id(subreddit: Subreddit, text: str) -> str | None:
     return next(
-        t["id"]
-        for t in subreddit.flair.link_templates
-        if t["type"] == "richtext"
-        and any(chunk.get("t") == text for chunk in t["richtext"])
+        (
+            t["id"]
+            for t in subreddit.flair.link_templates
+            if t["type"] == "richtext"
+            and any(chunk.get("t") == text for chunk in t["richtext"])
+        ),
+        None,
     )
 
 
